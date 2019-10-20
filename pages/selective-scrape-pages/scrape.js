76efb2965a1d960f474f0248707f14da25ac2aad
Ward Cunningham <ward@c2.com>
2019-10-20 14:58:16 -0700

    node app to interpret download

---
+++ pages/selective-scrape-pages/scrape.js
@@ -0,0 +1,43 @@
+// Convert graph download from scrape.html to svg
+// Usage: cat ~/Downloads/scrape-found.ward.bay.wiki.org-30.json | node scrape.js | dot -Tsvg > scrape.svg
+
+const fs = require('fs');
+const graph = JSON.parse(fs.readFileSync(0, 'utf-8'))
+
+function asSlug (title) {
+  return title.replace(/\s/g, '-')
+    .replace(/[^A-Za-z0-9-]/g, '')
+    .toLowerCase()
+}
+
+function resolve(node,link) {
+  let want = asSlug(link)
+  for (let probe of node.sites) {
+    if (graph[probe] && graph[probe][want]) {
+      return probe
+    }
+  }
+  return null
+}
+
+function quote(name) {
+  return `"${name}"`
+}
+
+console.log('strict digraph {')
+console.log('node [shape=box style=filled fillcolor=palegreen]')
+for (let site in graph) {
+  for (let slug in graph[site]) {
+    let node = graph[site][slug]
+    for (let link of node.links) {
+      let found = resolve(node, link)
+      if (found) {
+        console.log(quote(site), '->', quote(found))        
+      }
+    }
+    if (node.sites.length > 1) {
+      console.log(quote(site), '->', quote(node.sites[1]), '[style=dotted]')
+    }
+  }
+}
+console.log('}')
