972b413bbe9083c3b2deda15ea4bc3590b051b3f
Ward Cunningham <ward@c2.com>
2020-12-16 19:55:01 -0800

    revise lineup on click

--- pages/smallest-wiki-revisited/view.js
+++ pages/smallest-wiki-revisited/view.js
@@ -3,6 +3,7 @@
 
 export { start }
 
+const newpid = () => Math.floor(Math.random()*1000000)
 let lineup = window.lineup = []
 
 function start () {
@@ -24,11 +25,11 @@ function panels() {
   let fields = hash.split('/')
   let html = []
   while (fields.length) {
-    let [site,slug] = fields.splice(0,2)
-    let pid = Math.floor(Math.random()*1000000)
+    let [where,slug] = fields.splice(0,2)
+    let pid = newpid()
     html.push(`<article id=${pid}><h3>${slug}</h3></article>`)
-    let url = site=='view' ? `./${slug}.json` : `//${site}/${slug}.json`
-    let panel = {pid,site,slug,url}
+    let url = where=='view' ? `./${slug}.json` : `//${where}/${slug}.json`
+    let panel = {pid, where, slug, url}
     lineup.push(panel)
     fetch(url).then(res => res.json()).then(json => {panel.page = json; refresh(panel)})
   }
@@ -40,7 +41,7 @@ function footer() {
 }
 
 async function refresh(panel) {
-  let url = panel.site=='view' ? `./favicon.png` : `//${panel.site}/favicon.png`
+  let url = panel.where=='view' ? `./favicon.png` : `//${panel.where}/favicon.png`
   let title = `<h3><img width=24 src="${url}"> ${panel.page.title}</h3>`
   let story = panel.page.story.map(item => render(item,panel)).join("\n")
   document.getElementById(panel.pid).innerHTML = title+story
@@ -65,7 +66,7 @@ function render(item, panel) {
   }
 }
 
-function click(event) {
+async function click(event) {
   let target = event.target
   let pid = target.dataset.pid
   if (pid) {
@@ -73,7 +74,8 @@ function click(event) {
     event.stopPropagation()
     let title = target.innerText
     console.log({target, title, pid})
-    resolve(title, pid)
+    let panel = await resolve(title, pid)
+    console.log(panel)
   }
 }
 
@@ -81,11 +83,18 @@ async function resolve(title, pid) {
   const asSlug = (title) => title.replace(/\s/g, '-').replace(/[^A-Za-z0-9-]/g, '').toLowerCase()
   const recent = (list, action) => {if (action.site && !list.includes(action.site)) list.push(action.site); return list}
   let panel = lineup.find(panel => panel.pid == pid)
-  let path = (panel.page.journal||[]).reverse().reduce(recent,[location.host, panel.site])
+  let path = (panel.page.journal||[]).reverse().reduce(recent,[location.host, panel.where])
   console.log('resolve',{panel, path})
-  let probs = path.map(where => probe(where, asSlug(title)))
-  let pages = await Promise.all(probs)
+  let slug = asSlug(title)
+  let pages = await Promise.all(path.map(where => probe(where, slug)))
   console.log({path, pages})
+  let hit = pages.findIndex(page => page)
+  if (hit >= 0) {
+    return {pid:newpid(), where:path[hit], slug, page:pages[hit]}
+  } else {
+    let page = {title,story:[],journal:[]}
+    return {pid:newpid(), where:'ghost', slug, page}
+  }
 }
 
 function probe(where, slug) {
