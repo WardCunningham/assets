e7f97ccb42aa4db5145d0d56afdb78a7f43e5886
Ward Cunningham <ward@c2.com>
2020-12-22 22:44:27 -0800

    simplify octant calc

--- pages/workflow-automation/octant.js
+++ pages/workflow-automation/octant.js
@@ -1,14 +1,35 @@
 // https://en.wikipedia.org/wiki/Bearing_(navigation)
 // https://en.wikipedia.org/wiki/Octant_(plane_geometry)
+// http://www.csgnetwork.com/degreelenllavcalc.html
 
-let tab = []
+// http://trails.ward.asia.wiki.org/tuning-the-rose.html
+
 let lab = 'n,ne,e,se,s,sw,w,nw'.split(',')
-for (let ber=0; ber<=360; ber+=5) {
-  let east = Math.round(Math.sin(ber*Math.PI/180)*100)
-  let north = Math.round(Math.cos(ber*Math.PI/180)*100)
-  let angle = Math.round(180*Math.atan2(east,north)/Math.PI)
-  let oct = Math.floor((angle+360+45/2)/45%8)
+let tab = []
+
+for (let ber=0; ber<360; ber+=(45)) {
+
+  // test lat/lon in a circle radius one mile
+  let north =+(Math.sin((90-ber)*Math.PI/180)/69.05).toFixed(6)
+  let east = +(Math.cos((90-ber)*Math.PI/180)/-48.99).toFixed(6)
+
+  // compute planar geometry with math conventions
+  let dy = north*69.05
+  let dx = east*-48.99
+  let angle = Math.atan2(dy,dx)
+
+  // into clockwise bering
+  let bering = (90-angle*180/Math.PI+360)%360
+
+  // round to nearest octant
+  let oct = Math.round(bering/45)%8
   let dir = lab[oct]
-  tab.push({ber,east,north,angle,oct,dir})
+  tab.push({ber,north,east,dy,dx,angle,bering,oct,dir})
 }
 console.table(tab)
+
+// 45.4714296, -122.7462316 [[Hideaway Park]]
+let lat = 45.4714296
+let lon = -122.7462316
+let div = 1000
+console.log(tab.map(r => `${(lat+r.north).toFixed(6)}, ${(lon-r.east).toFixed(6)} ${r.ber} ⇒ ${r.dir}`).join("\n"))
