70e43161a02254391ea1f2245f46d4752b17c81b
Ward Cunningham <ward@c2.com>
2021-12-12 16:26:43 -0800

    quiet

--- pages/spark-records/wanted.sh
+++ pages/spark-records/wanted.sh
@@ -2,16 +2,16 @@
 # usage: sh wanted.sh 2 10
 
 ls data | tail -$1 | while read day; do
-  echo day $day
-  cat data/$day |\
-    jq -r '.[]|.time[0:15] +"0:00Z "' |\
-      sort | uniq | wc -l
+  # echo day $day
+  # cat data/$day |\
+  #   jq -r '.[]|.time[0:15] +"0:00Z "' |\
+  #     sort | uniq | wc -l
 
   export pattern=`cat data/$day |\
     jq -r '.[]|(.msg|capture("(?<a>[A-Z]+[0-9]+[A-Z]+)")|.a)' |\
       sort | uniq -c | sort -n | tail -$2 |\
         awk '{print $2}' | awk 'ORS="|"'`
-  echo $pattern
+  # echo $pattern
 
   cat data/$day |\
     jq -rj '.[]|(.time[0:15]),("0Z "),(.msg|capture("(?<a>'$pattern'K9OX)","n")|.a),("\n")' |\
