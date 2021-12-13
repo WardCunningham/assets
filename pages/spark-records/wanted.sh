5189723275ddf8abd0f6459ae32361b6f77747b0
Ward Cunningham <ward@c2.com>
2021-12-12 16:20:13 -0800

    most wanted stations

---
+++ pages/spark-records/wanted.sh
@@ -0,0 +1,20 @@
+# report 10 most wanted stations on a ten minute interval over last 2 days
+# usage: sh wanted.sh 2 10
+
+ls data | tail -$1 | while read day; do
+  echo day $day
+  cat data/$day |\
+    jq -r '.[]|.time[0:15] +"0:00Z "' |\
+      sort | uniq | wc -l
+
+  export pattern=`cat data/$day |\
+    jq -r '.[]|(.msg|capture("(?<a>[A-Z]+[0-9]+[A-Z]+)")|.a)' |\
+      sort | uniq -c | sort -n | tail -$2 |\
+        awk '{print $2}' | awk 'ORS="|"'`
+  echo $pattern
+
+  cat data/$day |\
+    jq -rj '.[]|(.time[0:15]),("0Z "),(.msg|capture("(?<a>'$pattern'K9OX)","n")|.a),("\n")' |\
+      egrep -v ':\d0Z *$' | sort | uniq -c | head -20
+
+done
