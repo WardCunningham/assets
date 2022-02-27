1528dbc1395fc44cb733cd032049a4fd817e7f2c
Ward Cunningham <ward@c2.com>
2022-02-27 08:21:16 -0800

    report unreached by fork

--- pages/fork-network-graph/roundabout.js
+++ pages/fork-network-graph/roundabout.js
@@ -5,16 +5,28 @@ import {Graph} from 'http://ward.dojo.fed.wiki/assets/pages/mock-graph-data/grap
 let g = await Graph.fetch('http://ward.dojo.fed.wiki/assets/pages/fork-network-graph/forks.json')
 console.log(g.tally())
 
-let n = g.n('Site',{name:Deno.args[0]||'dojo.fed.wiki'})
+let name = Deno.args[0]||'dojo.fed.wiki'
+console.log(name)
+
+let n = g.n('Site',{name})
 let p = n.o().t()
 let f = p.o().t()
-
 let t = []
-for (let i=7; i>0; i--) {
-  t.push({sites:n.props().length, pages:p.props().length, forks:f.props().length})
+const row = () => t.push({
+  sites:n.props().length,
+  pages:p.props().length,
+  forks:f.props().length
+})
+row()
+for (let i=6; i>0; i--) {
   n = f.o().t()
   p = n.o().t()
   f = p.o().t()
+  row()
 }
-
 console.table(t)
+
+let missing = g.n('Site').props()
+  .filter(name => name.endsWith('dojo.fed.wiki') && !n.props().includes(name))
+  .map(name => name.replace('.dojo.fed.wiki',''))
+console.log(missing)
