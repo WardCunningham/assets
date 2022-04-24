4e66471f6ce29021e753a70adc01e141de03cf8a
Ward Cunningham <ward@c2.com>
2022-04-24 06:59:52 -0700

    adjust rate by timestep

--- pages/mock-graph-data/pop-sim.js
+++ pages/mock-graph-data/pop-sim.js
@@ -1,12 +1,12 @@
 // duplicate vensim population model
 // usage: import {rate,state,step} from './pop-sim.js'
 
-export const rate = {lifetime:8, birthrate:0.125}
+export const rate = {lifetime:8, birthrate:0.125, timestep:0.125}
 export const state = {year:0, pop:1000}
 
 export function step() {
-  const births = state.pop * rate.birthrate
-  const deaths = state.pop / rate.lifetime
-  state.year += 0.125
+  const births = state.pop * (rate.birthrate * rate.timestep)
+  const deaths = state.pop / (rate.lifetime * rate.timestep)
+  state.year += rate.timestep
   state.pop += births-deaths
 }
