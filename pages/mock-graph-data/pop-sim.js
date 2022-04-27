20c7c3540e3ea422a27419e83d4266b59db96f11
Ward Cunningham <ward@c2.com>
2022-04-27 15:18:16 -0700

    correct and simplify scaling

--- pages/mock-graph-data/pop-sim.js
+++ pages/mock-graph-data/pop-sim.js
@@ -5,8 +5,8 @@ export const rate = {lifetime:8, birthrate:0.125, timestep:0.125}
 export const state = {year:0, pop:1000}
 
 export function step() {
-  const births = state.pop * (rate.birthrate * rate.timestep)
-  const deaths = state.pop / (rate.lifetime * rate.timestep)
+  const births = rate.timestep * (state.pop * rate.birthrate)
+  const deaths = rate.timestep * (state.pop / rate.lifetime)
   state.year += rate.timestep
   state.pop += births-deaths
 }
