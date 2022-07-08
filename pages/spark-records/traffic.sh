9ff3022685e2c9805eee42a14e8c6d6394cba678
Ward Cunningham <ward@c2.com>
2022-07-08 06:44:37 -0700

    cache aggregated traffic reports

--- pages/spark-records/traffic.sh
+++ pages/spark-records/traffic.sh
@@ -1,6 +1,17 @@
 # report total spots per band on a ten minute interval
 # usage: sh traffic.sh 2
 
-(cd data; cat `ls | tail -$1`) |\
-  jq -r '.[]|.time[0:15] +"0:00Z "+ (.tunedfrequency|tostring)[0:2]' |\
-  sort | uniq -c
+(cd data; ls | tail -$1 |\
+  while read d
+  do
+    if [[ ! -e ../cache/$d ]] || [[ $d -nt ../cache/$d ]]
+    then
+      # echo miss on $d >&2
+      cat $d | jq -r '.[]|.time[0:15] +"0:00Z "+ (.tunedfrequency|tostring)[0:2]' |\
+      sort | uniq -c | tee ../cache/$d
+    else
+      # echo hit on $d >&2
+      cat ../cache/$d
+    fi
+  done
+)
