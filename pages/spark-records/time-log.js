385a34c6abab1b747fc087d81beec5f2557e90a3
Ward Cunningham <ward@c2.com>
2022-07-08 06:30:01 -0700

    log only adjusted drift

--- pages/spark-records/time-log.js
+++ pages/spark-records/time-log.js
@@ -1,6 +1,8 @@
+await fetch(`http://${Deno.args[0]||'nr.local'}:8000`).then(res => res.text())
 setInterval(async () => {
   const t0 = Date.now()
-  const t1 = await fetch('http://3d.local:8000').then(res => res.text())
+  const t1 = await fetch(`http://${Deno.args[0]||'nr.local'}:8000`).then(res => res.text())
   const t2 = Date.now()
-  console.log(t0,t1-t0,t2-t0)
-},500)
+  const dt = t1-t0 - (t2-t0)
+  console.log(`${t0},${dt}`)
+},30000)
