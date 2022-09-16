a2535aad4e7ef4ebfc9b98531b48f5e1ae023b16
Ward Cunningham <ward@c2.com>
2022-09-15 17:04:40 -0700

    first version sticky-studio import

---
+++ pages/super-collaborator/tools/css.js
@@ -0,0 +1,19 @@
+// convert sticy-studio.json argument to Graph format
+// usage: deno run --allow-read http:hsc.fed.wiki/assets/css.js sample.json > sample.graph.json
+
+import {Graph} from 'https://wardcunningham.github.io/graph/graph.js'
+
+const input = await Deno.readTextFileSync(Deno.args[0])
+const data = JSON.parse(input)
+const g = new Graph()
+const nids = {}
+
+data.elements.forEach((e,i) => {
+  nids[e._id] = g.addNode('Element',{name:e.attributes.label})
+})
+data.connections.forEach((c,i) => {
+  g.addRel('Connection',nids[c.from],nids[c.to],{name:c.attributes.label})
+})
+
+console.error(g.tally())
+console.log(g.stringify(null,2))
