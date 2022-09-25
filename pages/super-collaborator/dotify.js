6838ef084a804c9468d3e804b3b22722382ba5a0
Ward Cunningham <ward@c2.com>
2022-09-25 15:22:51 -0700

    full list of props as tooltip

--- pages/super-collaborator/dotify.js
+++ pages/super-collaborator/dotify.js
@@ -2,13 +2,14 @@
 
 export function dotify(complex) {
   const {graph, merged} = complex
+  const tip = props => Object.entries(props).filter(e => e[1]).map(e => `${e[0]}: ${e[1]}`).join("\n")
   const nodes = graph.nodes.map((node,id) => {
     const icon = node.props.url ? " 🔗" : ""
     const label = `${node.type}\n${node.props.name}${icon}`
-    return `${id} [label="${label}" ${node.props.url?`URL="${node.props.url}" target="_blank"`:''}]`
+    return `${id} [label="${label}" ${node.props.url?`URL="${node.props.url}" target="_blank"`:''} tooltip="${tip(node.props)}"]`
   })
   const edges = graph.rels.map(rel => {
-    return `${rel.from}->${rel.to} [label="${rel.type}" labeltooltip="${rel.props.source}"]`
+    return `${rel.from}->${rel.to} [label="${rel.type}" labeltooltip="${tip(rel.props)}"]`
   })
   return [
     'digraph {',
