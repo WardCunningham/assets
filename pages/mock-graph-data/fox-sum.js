c501b1aaa7cbb26deab1600c7fec5871b4fdfa5e
Ward Cunningham <ward@c2.com>
2022-10-05 15:09:49 -0700

    sims finally match

--- pages/mock-graph-data/fox-sum.js
+++ pages/mock-graph-data/fox-sum.js
@@ -12,7 +12,7 @@ const graph = new Graph([
 {type:'Sum',in:[1],out:[2],props:{name:'Prey',value:100,wax:0.3}},
 {type:'Sum',in:[3],out:[0],props:{name:'Predator',value:10,wane:0.1}},
 ],[
-{type:'Decreases',from:1,to:0,props:{irate:.1,orate:.3}},
+{type:'Decreases',from:1,to:0,props:{irate:.1,orate:.1}},
 {type:'Increases',from:0,to:1,props:{irate:.002,orate:1}}
 ])
 
@@ -25,7 +25,7 @@ const graph = new Graph([
 const tab = []
 
 const bug = (name, value) => {
-  if (name.toLowerCase().startsWith('prey')) {
+  if (name.toLowerCase().startsWith('prey')||true) {
     tab[tab.length-1][name] = +value.toFixed(3)
   }
 }
@@ -35,12 +35,13 @@ const bugprops = (node, sufix, value) => {
 }
 
 sim.reset()
-for(let n=0; n<3; n++) {
+for(let n=0; n<360; n++) {
   const [prey,pred] = graph.nodes
   tab.push({})
   bug('prey_x', sim.calc('prey_x'))
-  bug('prey_inc_rate', sim.calc('prey_increase_rate'))
-  bug('prey_dec_rate', -sim.calc('prey_decrease_rate'))
+  // bug('prey_inc_rate', sim.calc('prey_increase_rate'))
+  // bug('prey_dec_rate', -sim.calc('prey_decrease_rate'))
+  // bug('prey_inc/dec', .125*(sim.calc('prey_increase_rate')-sim.calc('prey_decrease_rate')))
   bugprops(prey, '', calc(prey))
   bug('predators_y', sim.calc('predators_y'))
   bugprops(pred, '', calc(pred))
@@ -55,27 +56,25 @@ function calc(node) {
 }
 
 function step() {
-  const dt = 1 // 0.125
   const sign = {Increases: 1, Decreases: -1}
   const delta = graph.nodes.map(n => 0)
   for (const node of graph.nodes) {
     const nid = graph.nodes.findIndex(n => n === node)
-    delta[nid] += dt * (node.props.wax || 0) * calc(node)
-    delta[nid] -= dt * (node.props.wane || 0) * calc(node)
-    bugprops(node, ' Δ', delta[nid])
+    delta[nid] += (node.props.wax || 0) * calc(node)
+    delta[nid] -= (node.props.wane || 0) * calc(node)
+    // bugprops(node, ' Δ', delta[nid])
   }
   for (const rel of graph.rels) {
-    const bugrel = (sufix, value) => bugprops(graph.nodes[rel.to], sufix,  value)
-    const idelta = rel.props.irate * dt * calc(graph.nodes[rel.from])
-    const odelta = rel.props.orate * dt * calc(graph.nodes[rel.to])
-    delta[rel.to] += (idelta + odelta) * sign[rel.type]
-    bugrel(` iΔ`, idelta)
-    bugrel(` oΔ`, odelta)
-    bugrel(` ΔΔ`, delta[rel.to])
+    const idelta = rel.props.irate * calc(graph.nodes[rel.from])
+    const odelta = rel.props.orate * calc(graph.nodes[rel.to])
+    delta[rel.to] += (idelta * odelta) * sign[rel.type]
+    // const bugrel = (sufix, value) => bugprops(graph.nodes[rel.to], sufix,  value)
+    // bugrel(` iΔ`, idelta)
+    // bugrel(` oΔ`, odelta)
+    // bugrel(` ΔΔ`, delta[rel.to])
   }
-  console.log(delta)
   for (const nid in delta) {
-    graph.nodes[nid].props.value += delta[nid]
+    graph.nodes[nid].props.value += .125 * delta[nid]
   }
 }
 
