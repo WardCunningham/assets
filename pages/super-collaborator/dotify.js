41323a83da5e4b9991aa9c98c9d5d48d65dd6fad
Ward Cunningham <ward@c2.com>
2022-10-08 12:06:18 -0700

    svg nodes with checkboxes

--- pages/super-collaborator/dotify.js
+++ pages/super-collaborator/dotify.js
@@ -4,9 +4,9 @@ export function dotify(complex) {
   const {graph, merged} = complex
   const tip = props => Object.entries(props).filter(e => e[1]).map(e => `${e[0]}: ${e[1]}`).join("\n")
   const nodes = graph.nodes.map((node,id) => {
-    const icon = node.props.url ? " 🔗" : ""
+    const icon = node.props.url ? " 🔗" : node.props.tick ? " ☐" : ""
     const label = `${node.type}\n${node.props.name}${icon}`
-    return `${id} [label="${label}" ${node.props.url?`URL="${node.props.url}" target="_blank"`:''} tooltip="${tip(node.props)}"]`
+    return `${id} [label="${label}" ${(node.props.url||node.props.tick)?`URL="${node.props.url||'#'}" target="_blank"`:''} tooltip="${tip(node.props)}"]`
   })
   const edges = graph.rels.map(rel => {
     return `${rel.from}->${rel.to} [label="${rel.type}" labeltooltip="${tip(rel.props)}"]`
