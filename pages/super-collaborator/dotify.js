bd69d64899fcc63e722a52f2903ae605b0697d65
Ward Cunningham <ward@c2.com>
2022-10-29 22:36:54 -0700

    notice graphviz color in graph

--- pages/super-collaborator/dotify.js
+++ pages/super-collaborator/dotify.js
@@ -2,25 +2,33 @@
 
 export function dotify(complex) {
   const {graph, merged} = complex
-  const tip = props => Object.entries(props).filter(e => e[1]).map(e => `${e[0]}: ${e[1]}`).join("\n")
+  const tip = props => Object.entries(props).filter(e => e[1]).map(e => `${e[0]}: ${e[1]}`).join("\\n")
+
+  const layout = graph.nodes
+    .filter(node => node.type == 'Graphviz' && node.props.layout)
+    .map(node => node.props.layout)
+  const emphasis = graph.nodes
+    .filter(node => node.type == 'Graphviz' && node.props.emphasis)
+    .map(node => node.props.emphasis)
+    ?.reverse()[0] || {}
+  console.log('dotify',emphasis)
+
   const nodes = graph.nodes.map((node,id) => {
     const icon = node.props.url ? " 🔗" : node.props.tick ? " ☐" : ""
-    const label = `${node.type}\n${node.props.name}${icon}`
-    return `${id} [label="${label}" ${(node.props.url||node.props.tick)?`URL="${node.props.url||'#'}" target="_blank"`:''} tooltip="${tip(node.props)}"]`
+    const label = `${node.type}\\n${node.props.name}${icon}`
+    const color = emphasis[node.type] || ''
+    return `${id} [label="${label}" ${color} ${(node.props.url||node.props.tick)?`URL="${node.props.url||'#'}" target="_blank"`:''} tooltip="${tip(node.props)}"]`
   })
   const edges = graph.rels.map(rel => {
     return `${rel.from}->${rel.to} [label="${rel.type}" labeltooltip="${tip(rel.props)}"]`
   })
-  const layout = graph.nodes
-    .filter(node => node.type == 'Graphviz' && node.props.layout)
-    .map(node => node.props.layout)
   return [
     'digraph {',
     `layout = ${layout.reverse()[0]||'dot'}; overlap = false; splines=true`,
     'node [shape=box style=filled fillcolor=gold]',
     'rankdir=TB',
     ...merged.nids,
-    'node [fillcolor=lightgreen]',
+    'node [fillcolor=palegreen]',
     ...nodes,
     ...edges,
     '}'].join("\n")
