976477bb36d90d85c3ce2929933de5cb0599ed78
Ward Cunningham <ward@c2.com>
2022-11-10 17:01:36 -0800

    handle drop of urls

--- pages/super-collaborator/drop.js
+++ pages/super-collaborator/drop.js
@@ -33,6 +33,20 @@ export async function dropl (event,sufix) {
   return concepts
 }
 
+export async function dropu (event) {
+  const want = strings(event)
+  const concepts = []
+  for (const file of want) {
+    const filename = await file
+    const name = filename.split(/\//).reverse()[0].split(/\./)[0]
+    const graph = await fetch(filename)
+      .then(res => res.json())
+      .then(inst)
+    concepts.push({name,graph})
+  }
+  return concepts
+}
+
 function files(event) {
   if (event.dataTransfer.items) {
     return [...event.dataTransfer.items]
@@ -41,4 +55,16 @@ function files(event) {
   } else {
     return [...event.dataTransfer.files]
   }
+}
+
+function strings(event) {
+  if (event.dataTransfer.items) {
+    return [...event.dataTransfer.items]
+      .filter(item => item.kind === 'string')
+      .filter(item => item.type === 'text/uri-list')
+      .map(item => {return new Promise(res => item.getAsString(res))})
+  } else {
+    return [...event.dataTransfer.files]
+  }
+
 }
