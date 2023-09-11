c1533581e7c18c676771b2daabfd4e201cd10d9d
Ward Cunningham <ward@c2.com>
2023-09-10 20:53:07 -0700

    write and read register.txt

--- home/crazy.fed.wiki/register.js
+++ home/crazy.fed.wiki/register.js
@@ -2,10 +2,17 @@
 
 import * as fs from 'node:fs';
 import * as path from 'node:path';
+const status = '/Users/ward/.wiki/localhost/status'
 
 export function post(data, done) {
-  const status = '/Users/ward/.wiki/localhost/status'
-  const dir = fs.readdirSync(status)
-  console.log(dir)
-  done(null, 'ok')
+  const email = data?.email || 'none'
+  fs.appendFileSync('./require.txt', `${email}\n`);
+  const emails = fs.readFileSync('./require.txt') || ""
+  const count = emails.trim().split(/\n/).length
+  return `${count} members in queue\n`
+}
+
+export function get(done) {
+  const emails = fs.readFileSync('./require.txt')
+  return (emails||'none')
 }
