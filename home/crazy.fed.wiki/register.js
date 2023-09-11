5eae9bad45064267db9bf7729f80202e4f2e9e07
Ward Cunningham <ward@c2.com>
2023-09-11 16:44:03 -0700

    improved api and correct subscription log file name

--- home/crazy.fed.wiki/register.js
+++ home/crazy.fed.wiki/register.js
@@ -4,15 +4,16 @@ import * as fs from 'node:fs';
 import * as path from 'node:path';
 const status = '/Users/ward/.wiki/localhost/status'
 
-export function post(data, done) {
+export function post(argv, data, done) {
   const email = data?.email || 'none'
-  fs.appendFileSync('./require.txt', `${email}\n`);
-  const emails = fs.readFileSync('./require.txt') || ""
+  fs.appendFileSync(path.join(argv.status,'register.txt'), `${email}\n`);
+  const emails = fs.readFileSync(path.join(argv.status,'register.txt'),'utf8') || ""
+  console.log({emails})
   const count = emails.trim().split(/\n/).length
-  return `${count} members in queue\n`
+  done(null,`${count} members in queue\n`)
 }
 
-export function get(done) {
-  const emails = fs.readFileSync('./require.txt')
-  return (emails||'none')
+export function get(argv, done) {
+  const emails = fs.readFileSync(path.join(argv.status,'register.txt'),'utf8')
+  done(null, emails||'none')
 }
