8272a3143442cf422d48c032865ac799b03b0f2a
Ward Cunningham <ward@c2.com>
2023-09-18 11:08:42 -0700

    remove unused const

--- home/crazy.fed.wiki/register.js
+++ home/crazy.fed.wiki/register.js
@@ -1,14 +1,12 @@
-// sample register module for custom demo
+// example register.js module for crazy.fed.wiki demo
 
 import * as fs from 'node:fs';
 import * as path from 'node:path';
-const status = '/Users/ward/.wiki/localhost/status'
 
 export function post(argv, data, done) {
   const email = data?.email || 'none'
   fs.appendFileSync(path.join(argv.status,'register.txt'), `${email}\n`);
   const emails = fs.readFileSync(path.join(argv.status,'register.txt'),'utf8') || ""
-  console.log({emails})
   const count = emails.trim().split(/\n/).length
   done(null,`${count} members in queue\n`)
 }
