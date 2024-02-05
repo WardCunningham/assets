524a93bd4e9c5e838ace669d2d0bb1c408fd5a8b
Ward Cunningham <ward@c2.com>
2024-02-05 14:18:25 -0800

    add js version to run from express

---
+++ pages/esp8266-datalog/gaps.js
@@ -0,0 +1,24 @@
+// Find and report gaps in sensor logs
+// Usage: deno run --allow-net gaps.js
+
+  const path = `plugin/datalog/esp8266-datalog/day`
+  const site = `http://found.ward.bay.wiki.org/${path}`
+  const offsets = [9,8,7,6,5,4,3,2,1,0]
+  const report = []
+  const logs = offset =>
+    fetch(`${site}/${offset}`)
+      .then(res => res.text())
+      .then(text => text.trim().split(/\n/))
+  const data = (await Promise.all(offsets.map(logs))).flat()
+  let now = JSON.parse(data.shift()).clock
+  const keys = result => result
+    .map(obj => Object.keys(obj))
+    .flat()
+  for(const text of data) {
+    const sample = JSON.parse(text)
+    if(!keys(sample.result).length) continue
+    const dt = sample.clock-now
+    if(dt>20000) report.push({start:now, duration:dt})
+    now = sample.clock
+  }
+  console.log(report)
